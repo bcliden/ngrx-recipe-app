f5e96db21b2ec4ca5810bbb96e708cd9fa10fa05
Benjamin Liden <benjamin.c.liden@gmail.com>
2019-02-18 21:25:33 -0500

    added Login/Logout QOL. redirect, logged in: label, and more

--- src/app/components/auth/auth.component.ts
+++ src/app/components/auth/auth.component.ts
@@ -1,37 +1,51 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { Store } from "@ngrx/store";
+import { takeUntil } from "rxjs/operators";
 
 import { AppState } from "@app/store/app-store.module";
 import { validateWhitespace } from "@app/utility/validators";
-import {
-  LoginUser,
-  RegisterUser,
-  SetInitialUser
-} from "@app/store/actions/auth.actions";
+import { LoginUser, RegisterUser } from "@app/store/actions/auth.actions";
 import { AuthDTO } from "@app/models/auth";
-import { AuthService } from "@app/services/auth.service";
+import { Subject } from "rxjs";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-auth",
   templateUrl: "./auth.component.html",
   styleUrls: ["./auth.component.scss"]
 })
-export class AuthComponent implements OnInit {
+export class AuthComponent implements OnInit, OnDestroy {
   authForm: FormGroup;
+  loading: boolean = false;
+  unsubscribe$ = new Subject<void>();
 
-  // authForm = new FormGroup({
-  //   username: new FormControl("", [Validators.required]),
-  //   password: new FormControl("", [Validators.required])
-  // });
-
-  constructor(private fb: FormBuilder, private store: Store<AppState>) {}
+  constructor(
+    private fb: FormBuilder,
+    private store: Store<AppState>,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     this.authForm = this.fb.group({
       username: this.fb.control("", [Validators.required, validateWhitespace]),
       password: this.fb.control("", [Validators.required, validateWhitespace])
     });
+
+    this.store
+      .select(state => state.auth)
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe(val => {
+        this.loading = val.loading;
+        if (val.user && val.loading !== true) {
+          this.router.navigate(["/"]);
+        }
+      });
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 
   login() {
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,13 +1,22 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
 import { AuthService } from "@app/services/auth.service";
 import { MenuItem } from "primeng/components/common/menuitem";
+import { SetCurrentUser } from "@app/store/actions/auth.actions";
+import { AppState } from "@app/store/app-store.module";
+import { Store } from "@ngrx/store";
+import { Router } from "@angular/router";
+import { User } from "@app/models/user";
+import { Observable, Subject } from "rxjs";
+import { tap, takeUntil, filter } from "rxjs/operators";
 
 @Component({
   selector: "app-navbar",
   templateUrl: "./navbar.component.html",
   styleUrls: ["./navbar.component.scss"]
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, OnDestroy {
+  unsubscribe$ = new Subject<void>();
+  username: string | null;
   items: MenuItem[] = [
     {
       label: "Home",
@@ -26,7 +35,33 @@ export class NavbarComponent implements OnInit {
     }
   ];
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private store: Store<AppState>,
+    private router: Router
+  ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    const user$ = this.store
+      .select(state => state.auth.user && state.auth.user.username)
+      .pipe(
+        // filter(v => v !== null),
+        // tap(v => console.log("hi ", v)),
+        takeUntil(this.unsubscribe$)
+      )
+      .subscribe(val => (this.username = val));
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
+
+  onClick() {
+    if (this.authService.token) {
+      this.authService.token = null;
+      this.store.dispatch(new SetCurrentUser(null));
+    }
+    this.router.navigate(["/auth"]);
+  }
 }
